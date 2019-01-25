ed4ad24f514d9527e8c82bf740b89bab288e8a45
Samuel Cornet <samuel@noukod.com>
2019-01-25 16:15:13 +0000

    Automatically backed up by Learn

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,8 +1,8 @@
 class Doctor < ActiveRecord::Base
   has_many :patients
   has_many :patients, through: :appointments
-
-  def patient_count
-    patients.count
-  end
+  #
+  # def patient_count
+  #   patients.count
+  # end
 end
